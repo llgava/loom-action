2456704c9e989281c8c3a19ee043431a62fb83ae
Leonardo Luiz Gava <llgavamt@gmail.com>
2023-01-14 10:23:56 -0400

    add: babel

--- src/LoomAction.ts
+++ src/LoomAction.ts
@@ -1,14 +1,18 @@
 import core from '@actions/core';
 import { PatternReader } from './PatternReader';
+import { BehaviorPack } from './types/BehaviorPack';
+import { Pack } from './types/Pack';
 
 export class LoomAction {
-  public static pattern: string = core.getInput('pattern');
-  public static bpPath: string = core.getInput('behavior_pack_path');
-  public static rpPath: string = core.getInput('resource_pack_path');
+  public static pattern: string = 'mojang' /* core.getInput('pattern') */;
+  public static bpPath: string = './behavior_packs/0' /* core.getInput('behavior_pack_path') */;
+  public static rpPath: string = './resource_packs/0' /* core.getInput('resource_pack_path') */;
 
   public static reader: PatternReader = new PatternReader(this.pattern);
 
   public static run(): void {
+    console.clear();
+    this.reader.getFileEndingFrom(Pack.BEHAVIOR_PACK, BehaviorPack.ANIMATION_CONTROLLERS);
     console.log('Hello world');
   }
 }
--- src/patterns/index.ts
+++ src/patterns/index.ts
@@ -1,4 +1,4 @@
 import path from 'path';
 
 export const mojang = path.join(__dirname, 'mojang.yml');
-export const custom = path.join(__dirname, '..', '..', 'pattern.yml');
+export const custom = path.join(__dirname, '..', 'pattern.yml');
