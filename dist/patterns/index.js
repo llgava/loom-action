fb8fe21b8bbbabab28e5cfbac75cba5a7a42891e
Leonardo Luiz Gava <llgavamt@gmail.com>
2023-01-15 01:53:36 -0400

    add: custom pattern workflow

--- dist/patterns/index.js
+++ dist/patterns/index.js
@@ -8,5 +8,5 @@ var _path = _interopRequireDefault(require("path"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var mojang = _path["default"].join(__dirname, 'mojang.yml');
 exports.mojang = mojang;
-var custom = _path["default"].join(__dirname, '..', 'pattern.yml');
+var custom = _path["default"].join(__dirname, '..', '..', 'pattern.yml');
 exports.custom = custom;
